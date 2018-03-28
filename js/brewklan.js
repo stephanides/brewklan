cba1bcaaa17637702aa5884bbf80b52c4fe8610e
Štefan Mačuga <stefanmacuga1@gmail.com>
2018-03-28 23:07:36 +0200

    28.3

--- js/brewklan.js
+++ js/brewklan.js
@@ -4,3 +4,23 @@ if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobi
 else{
 
 }
+function section(e){
+  $('html, body').animate({scrollTop:$(`#section-${e}`).position().top}, 'slow');
+}
+function sectionMobile(e){
+  document.getElementById("menuMobile").style.top = "-200vh";
+  document.getElementById("closeMenuMobileIcon").style.display = "none";
+  document.getElementById("menuMobileIcon").style.display = "block";
+  $('html, body').animate({scrollTop:$(`#section-${e}`).position().top - $('.header').outerHeight()}, 'slow');
+
+}
+function showMenuMobile(){
+  document.getElementById("menuMobile").style.top = "0px";
+  document.getElementById("closeMenuMobileIcon").style.display = "block";
+  document.getElementById("menuMobileIcon").style.display = "none";
+}
+function hideMenuMobile(){
+  document.getElementById("menuMobile").style.top = "-200vh";
+  document.getElementById("closeMenuMobileIcon").style.display = "none";
+  document.getElementById("menuMobileIcon").style.display = "block";
+}
